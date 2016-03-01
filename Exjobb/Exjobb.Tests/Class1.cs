1ce9e7e1cb50ac9873ee6f4dd96107604d4412a7
Filip Ferm <filip.ferm@starrepublic.com>
2016-03-01 11:02:11 +0100

    FirstCommit

---
+++ Exjobb/Exjobb.Tests/Class1.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exjobb.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        [Ignore("Dummy test, Remove this")]
+        public void dummy()
+        {
+
+        }
+    }
+}
